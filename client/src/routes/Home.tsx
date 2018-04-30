1498e942bf3fcc5783856b6cba50ae87e174bf04
unknown <hansel809@hotmail.com>
2018-04-30 12:50:37 -0400

    Finished home

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -2,15 +2,14 @@ import * as React from 'react';
 import { Container, Header, Segment } from 'semantic-ui-react';
 import locale from '@app/core/locale';
 
-const image =
-  'https://christianchronicle.org/wp-content/uploads/2017/09/home-768x431.jpg';
-
 const Home: React.SFC = () => (
   <div>
     <Segment inverted>
       <Header as="h2">{locale.home}</Header>
       <Container textAlign="center">
-        <img src={image} alt="home" width={550} />
+        <Segment inverted>
+          <p>No notifications at this time</p>
+        </Segment>
       </Container>
     </Segment>
   </div>
